95960cb567bf500374ec2e9d4dac7582b1555e86
Danilo Giacomini <me@daenschi.ch>
2026-09-24 14:18:49 +0100

    Adapt Compat to nvim-hs main's typed API bindings
    nvim-hs main (2.3.2.7) parses Neovim 0.12's api-info correctly and
    generates typed bindings (Vector Window, (Int64, Int64), Vector Text).
    The previous Compat wrappers expected the untyped Vector Object fallback
    produced by nvim-hs 2.3.2.4 with pre-release 0.12 api-info metadata.

--- src/Cornelis/Vim/Compat.hs
+++ src/Cornelis/Vim/Compat.hs
@@ -1,7 +1,6 @@
 module Cornelis.Vim.Compat where
 
-import Control.Monad.IO.Class (MonadIO)
-import Data.Either
+import Data.Int (Int64)
 import Data.Text (Text)
 import Data.Vector (Vector)
 import qualified Data.Vector as Vector
@@ -9,33 +8,29 @@ import Neovim
 import Neovim.API.Text (Buffer, Window)
 import qualified Neovim.API.Text as Vim
 
-unwrapArray :: (NvimObject o) => Vector Object -> [o]
-unwrapArray = rights . Vector.toList . fmap fromObject
-
-wrapArray :: (NvimObject o) => [o] -> Vector Object
-wrapArray = Vector.fromList . fmap toObject
-
-toPair :: (MonadIO io, NvimObject o1, NvimObject o2) => [Object] -> io (o1, o2)
-toPair = fromObject' . ObjectArray
-
-fromPair :: (NvimObject o1, NvimObject o2) => (o1, o2) -> Vector Object
-fromPair (x, y) = Vector.fromList [toObject x, toObject y]
+-- This branch builds against nvim-hs @ main (2.3.2.7), whose API code
+-- generation understands Neovim 0.12's api-info format and produces TYPED
+-- bindings (Vector Window, (Int64, Int64), Vector Text, ...).
+-- The original nvim-0.12 branch expected the untyped "Vector Object"
+-- fallback that nvim-hs 2.3.2.4 produced for pre-release 0.12 api-info.
 
 nvim_list_wins :: Neovim env [Window]
-nvim_list_wins = unwrapArray <$> Vim.nvim_list_wins
+nvim_list_wins = Vector.toList <$> Vim.nvim_list_wins
 
 window_get_cursor :: Window -> Neovim env (Int, Int)
-window_get_cursor = (toPair . Vector.toList =<<) . Vim.window_get_cursor
+window_get_cursor w = do
+    (r, c) <- Vim.window_get_cursor w
+    pure (fromIntegral r, fromIntegral c)
 
 window_set_cursor :: Window -> (Int, Int) -> Neovim env ()
-window_set_cursor w = Vim.window_set_cursor w . fromPair
+window_set_cursor w (r, c) = Vim.window_set_cursor w (fromIntegral r, fromIntegral c)
 
 nvim_buf_get_lines :: Buffer -> Int64 -> Int64 -> Bool -> Neovim env [Text]
-nvim_buf_get_lines b s e flag = unwrapArray <$> Vim.nvim_buf_get_lines b s e flag
+nvim_buf_get_lines b s e flag = Vector.toList <$> Vim.nvim_buf_get_lines b s e flag
 
 nvim_buf_get_lines' :: Buffer -> Int64 -> Int64 -> Bool -> Neovim env (Vector Text)
-nvim_buf_get_lines' b s e flag = Vector.fromList <$> nvim_buf_get_lines b s e flag
+nvim_buf_get_lines' = Vim.nvim_buf_get_lines
 
 nvim_buf_set_text ::
     Buffer -> Int64 -> Int64 -> Int64 -> Int64 -> [Text] -> Neovim env ()
-nvim_buf_set_text b sl sc el ec = Vim.nvim_buf_set_text b sl sc el ec . wrapArray
+nvim_buf_set_text b sl sc el ec = Vim.nvim_buf_set_text b sl sc el ec . Vector.fromList
